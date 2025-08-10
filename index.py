19879ca1f5305b6c998dca3720c80477c76c69bb
campagnoli <33726078+campagnoli@users.noreply.github.com>
2025-08-10 08:09:26 -0300

    Delete index.py

--- index.py
+++
@@ -1,9 +0,0 @@
-```python
-from src.main import app
-
-# Vercel precisa de uma função handler
-def handler(request):
-    return app(request.environ, start_response)
-
-if __name__ == "__main__":
-    app.run()
