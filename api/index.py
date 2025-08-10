1ee0048e168e0a7120a294274ba465fc5fcc6d3c
campagnoli <33726078+campagnoli@users.noreply.github.com>
2025-08-10 08:51:18 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+# Adiciona o diretório 'src' ao PYTHONPATH
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src'))
+
 from src.main import app
 
 # Vercel precisa de uma função handler que se comporte como uma aplicação WSGI
